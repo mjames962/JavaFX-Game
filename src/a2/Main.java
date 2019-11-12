f1655aee09c227799e07f293b14afe1f38387d0d
Jensen_Beard <jensenbeard00@gmail.com>
2019-11-12 12:10:47 +0000

    Removed the mess from the end of the message

--- src/a2/Main.java
+++ src/a2/Main.java
@@ -1,10 +1,14 @@
 package a2;
 
+/**
+ * @author Jensen
+ *
+ */
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("help me                                                  AS vkewjbvwekjBVsdkjbvdkSJBVldbvjskdcbvkadsjcvkjadcvKVJBdcaksjcbkajscnvAKJScnkjav;dkajvkjasdvnkjDVNKJLbvKJCNKJASVKAJDSVKAJCSNKJACSNKJavnKJASVKAJCNKASJCNKAJCNKAJCNAkjCNAKJVNdsk;h;DSJVHs;dkjvnW;RLKVNHaCSJBGFSILUBHGE;SOING;JDFSBN;OAISEFNHDSKJVDBSNGJbsw;oderhnvblskcnawiqfjhasklnvEWLGN;AoinVd;ofjn;jcvW;EGKJNV;oadi");
+		System.out.println("help me   ");
 	}
 
 }
