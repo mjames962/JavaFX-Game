3ef95475d0a6ff2f0dc72a1eca63dffc0d150b98
Jensen_Beard <jensenbeard00@gmail.com>
2019-11-12 12:03:18 +0000

    Added "help me" test message

--- src/a2/Main.java
+++ src/a2/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("help me                                                  AS vkewjbvwekjBVsdkjbvdkSJBVldbvjskdcbvkadsjcvkjadcvKVJBdcaksjcbkajscnvAKJScnkjav;dkajvkjasdvnkjDVNKJLbvKJCNKJASVKAJDSVKAJCSNKJACSNKJavnKJASVKAJCNKASJCNKAJCNKAJCNAkjCNAKJVNdsk;h;DSJVHs;dkjvnW;RLKVNHaCSJBGFSILUBHGE;SOING;JDFSBN;OAISEFNHDSKJVDBSNGJbsw;oderhnvblskcnawiqfjhasklnvEWLGN;AoinVd;ofjn;jcvW;EGKJNV;oadi");
 	}
 
 }
