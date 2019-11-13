69c8398f09d1d140d664b84ce88ac74df1d43722
jensen <jensen@SteamPunkMobile>
2019-11-13 19:02:43 +0000

    Added some Javadoc with checkstyle working on my laptop

--- src/a2/Main.java
+++ src/a2/Main.java
@@ -2,10 +2,14 @@ package a2;
 
 /**
  * @author Jensen
- *
+ * Class containing the Main Method.
  */
 public class Main {
 	
+	/**
+	 * Main Method.
+	 * @param args Don't remember
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("help me   ");
