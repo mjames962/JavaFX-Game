ade96afcbe560d0689c64c9ba3269bb250f998fc
Jensen_Beard <jensenbeard00@gmail.com>
2019-11-15 12:53:33 +0000

    Updated Main, Created FXML file and Level.
    Began working on file reader.
    MAIN AND MAINMENU.FXML DO NOT WORK.

---
+++ src/a2/Level.java
@@ -0,0 +1,74 @@
+package a2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Level {
+	
+	
+	
+	
+	public static Cell[][] readFile(String fileName) {
+		File inputFile = new File(fileName);
+
+		Scanner in = null;
+		try {
+			in = new Scanner(inputFile);
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Cannot open '" + fileName + "'");
+			System.exit(0);
+		}
+
+		return Level.readFile(in);
+	}
+	
+	public static Cell[][] readFile(Scanner in){
+		
+		int xLength = in.nextInt();
+		int yLength = in.nextInt();
+		
+		Cell[][] level = new Cell[xLength][yLength];
+		
+		in.nextLine();
+		
+		for (int i = yLength; i > 0; i-- ) {
+			for (int j = 0; j < xLength; j++) {
+				level[j][i] = new Cell();
+			}
+		}
+
+		in.nextLine();
+		
+		
+		while (in.hasNextLine()) {
+			String temp = in.nextLine();
+			while(!temp.equals("*")) {
+				readEntity(in);
+				temp = in.nextLine();
+			}
+			
+			
+			temp = in.nextLine();
+			if (!temp.equals("*")) {
+				//
+			}
+			
+		}
+
+
+		return null;
+	}
+	
+	public static void readEntity(Scanner in) {
+		int startX = in.nextInt();
+		int startY = in.nextInt();
+		int entityID = in.nextInt();
+		
+		in.useDelimiter("~");
+		
+		String direction = in.next();
+		
+	}
+}
--- src/a2/Main.java
+++ src/a2/Main.java
@@ -1,22 +1,42 @@
 package a2;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
 /**
- * Class containing the Main Method.
- * @author Jensen
+ * The main class for this program.
+ * @author Jensen Mitch
+ *
  */
-public class Main {
+public class Main extends Application {
+	// Constants for the main window
+	private static final int MAIN_WINDOW_WIDTH = 600;
+	private static final int MAIN_WINDOW_HEIGHT = 400;
+	private static final String WINDOW_TITLE = "PlaceHolder";
 	
-	/**
-	 * Main Method.
-	 * @param args Don't remember
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		System.out.println("help me   ");
-		System.out.println("test");
-		System.out.println("second test");
-		
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Load the main scene.
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("C:/Users/Jensen/git/a2/src/a2/resources/MainMenu.fxml"));
+			Scene scene = new Scene(root, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+	
+			// Place the main scene on stage and show it.
+					primaryStage.setScene(scene);
+					primaryStage.setTitle(WINDOW_TITLE);
+					primaryStage.show();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+			public static void main(String[] args) {
+				launch(args);
+			}
 	}
+	
 
-}
-
+	
