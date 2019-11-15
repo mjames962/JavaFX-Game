c71db9fb06a1aa719492107591eb29e908b6adda
dktho <dktho@137.44.128.122>
2019-11-15 12:37:14 +0000

    Added things

--- src/a2/Main.java
+++ src/a2/Main.java
@@ -1,22 +1,5 @@
 package a2;
 
-/**
- * Class containing the Main Method.
- * @author Jensen
- */
 public class Main {
 	
-	/**
-	 * Main Method.
-	 * @param args Don't remember
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		System.out.println("help me   ");
-		System.out.println("test");
-		System.out.println("second test");
-		
-	}
-
 }
-
