bed0dc9f7b533ef78ed7bfffa931119e7bde91ad
Jensen_Beard <jensenbeard00@gmail.com>
2019-11-22 11:50:36 +0000

    Created log in checker to ensure the user exists before logging in.

--- src/a2/CreateUserController.java
+++ src/a2/CreateUserController.java
@@ -1,5 +1,14 @@
 package a2;
 
+/**
+ * @author Jensen
+ *
+ */
 public class CreateUserController {
-
+	if (!UserData.doesExist(tbox_.getText())) {
+		// create user
+	} else {
+		//error message 
+	}
+	
 }
---
+++ src/a2/UserMainTest.java
@@ -0,0 +1,14 @@
+package a2;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class UserMainTest {
+
+	public static void main(String[] args) {
+		
+
+
+	}
+
+}
