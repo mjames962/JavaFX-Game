a59918560741dfaa213d1022b65d7449c8d61a2e
Jensen <Jensen@CyberPunk>
2019-11-29 00:01:23 +0000

    Removed unneeded imports

--- src/a2/MainMenuController.java
+++ src/a2/MainMenuController.java
@@ -1,16 +1,12 @@
 package a2;
 
 
-import java.io.IOException;
+import java.io.IOException; 
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
