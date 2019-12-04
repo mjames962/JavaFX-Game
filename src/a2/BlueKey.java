363ee72bc65ec446e8130cc9fe1d7558f603cb73
James Colebourn <afroyodacolebourn@gmail.com>
2019-12-04 12:20:58 +0000

    JDoc done

--- src/a2/BlueKey.java
+++ src/a2/BlueKey.java
@@ -1,22 +1,29 @@
 package a2;
 /**
  * the item blue key for the players inventory.
- * @author George, Tom
+ * @author George Williams Walton, Tom Wood
  */
 public class BlueKey extends Key {
-	
-	private static final String SPRITE = "a2/resources/stock photos/BlueKeySprite.png";
+	private static final int ID = 3;
+	private static final String 
+		SPRITE = "a2/resources/stock photos/BlueKeySprite.png";
 	/**
 	 * An item of type blue key.
 	 */
 	public BlueKey() {
 		super();
 	}
-	
+	/**
+	 * Returns the itemID.
+	 * @return gives the itemID of 3
+	 */
 	public int getItemID() {
-		return 3;
+		return ID;
 	}
-	
+	/**
+	 * Returns the sprite.
+	 * @return gives the sprite
+	 */
 	public String getSprite() {
 		return SPRITE;
 	}
