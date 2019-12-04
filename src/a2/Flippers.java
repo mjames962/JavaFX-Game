37dc064de07e3cb1446595ba464dcd8425500dd1
James Colebourn <afroyodacolebourn@gmail.com>
2019-12-04 12:50:28 +0000

    JDoc done for above classes

--- src/a2/Flippers.java
+++ src/a2/Flippers.java
@@ -1,19 +1,32 @@
 package a2;
 
+/**
+ * The Class responsible for the Flippers Item to 
+ * 							be stored in the player's inventory.
+ * @author George Williams Walton, Tom Wood
+ *
+ */
 public class Flippers extends Item {
-	
-	private static final String SPRITE = "a2/resources/stock photos/flippers.png";
+	private static final int ID = 5;
+	private static final String SPRITE = 
+			"a2/resources/stock photos/flippers.png";
 
 	@Override
 	void pickUp(Player p) {
 		// TODO Auto-generated method stub
 		
 	}
-	
+	/**
+	 * Returns the ItemID for Flippers.
+	 * @return gives the ItemID
+	 */
 	public int getItemID() {
-		return 5;
+		return ID;
 	}
-	
+	/**
+	 * Returns the Flippers sprite.
+	 * @return gives the sprite for the Item
+	 */
 	public String getSprite() {
 		return SPRITE;
 	}
