a2676706250a7777b33b6529e11a4055db76ce55
A Mr Nobody <46794914+A-Mr-Nobody@users.noreply.github.com>
2019-12-04 14:53:24 +0000

    Fixed alert

--- src/a2/LevelSelectController.java
+++ src/a2/LevelSelectController.java
@@ -12,6 +12,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
