056b0c3c0e4b841f6684364aa153ad780a720c6f
A Mr Nobody <46794914+A-Mr-Nobody@users.noreply.github.com>
2019-12-04 14:49:27 +0000

    Working + leaderboard stuff

--- src/a2/LeaderboardController.java
+++ src/a2/LeaderboardController.java
@@ -1,9 +1,15 @@
 package a2;
 
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.adapter.JavaBeanStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -28,7 +34,7 @@ public class LeaderboardController {
 	@FXML 
 	private Button leadBut3;
 	@FXML 
-	private ChoiceBox levelSelector;
+	private ChoiceBox<String> levelSelector;
 	@FXML 
 	private TableView<LeaderboardEntry> leaderTable;
 
@@ -37,12 +43,66 @@ public class LeaderboardController {
 	 * Displays the leaderboard.
 	 */
 	public void display() {
-		
+		addValuesToSelector();
 		Leaderboard lead = UserData.readLeaderboard(1);
 		if (lead != null) {
 			lead.display(leaderTable);
 		}
+		hookSelector();
+		
         
+	}
+	
+	
+	
+	public void hookSelector() {
+		levelSelector.getSelectionModel().
+		selectedItemProperty().addListener(new ChangeListener<String>() {
+		      @Override
+		      public void changed(ObservableValue<? extends String> observableValue,String oldVal, String newVal) {
+		    	  System.out.println(oldVal + newVal);
+		    	  System.out.println(UserData.getLevelNumber(newVal));
+		    	  changeDisplayedLeaderboard(UserData.getLevelNumber(newVal));
+		      }
+		});
+	}
+	
+	public void changeDisplayedLeaderboard(int levelNo) {
+		Leaderboard lead = UserData.readLeaderboard(levelNo);
+		if (lead != null) {
+			scrubTableView(leaderTable);
+			lead.display(leaderTable);
+		}
+	}
+	
+	public void scrubTableView(TableView tv) {
+		tv.getItems().clear();
+		tv.getColumns().clear();
+		
+	}
+
+	public void addValuesToSelector() {
+		File folder = new File(UserData.LEVEL_FOLDER_LOCATION);
+		File[] listOfFiles = folder.listFiles();
+		System.out.println("file testing");
+		for (File file : listOfFiles) {
+			String fileName = file.getName();
+			
+			//gets level number from file name
+			String levelString = fileName.substring(0, fileName.length() - 4); 
+			//String levelNumString = fileName.replaceFirst("([0-9]+)\\.txt", "");
+			int levelNum;
+			Matcher matcher = Pattern.compile("([0-9]+)\\.txt").matcher(fileName);
+			if (matcher.find()) {
+				levelNum =  Integer.parseInt(matcher.group(1));
+				System.out.println(levelNum);
+			} else {
+				levelNum = -1;
+			}		
+			if (file.isFile()) {
+		    	levelSelector.getItems().add(UserData.getLevelIdentifier(file));
+		    }
+		}
 	}
 	/**
 	 * Allows for pressing buttons.
