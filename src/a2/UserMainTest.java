0f4fbe14a72fb2f00ffbd3917b9465494a2b455d
981988 <981988@ISS-P1210334274.tawe.swan.ac.uk>
2019-12-06 11:51:14 +0000

    merge stuff again lol

--- src/a2/UserMainTest.java
+++ src/a2/UserMainTest.java
@@ -1,5 +1,8 @@
 package a2;
 
+import java.io.File;
+import java.io.IOException;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
@@ -7,6 +10,20 @@ public class UserMainTest {
 
 	public static void main(String[] args) {
 
+	Level level = new Level(Level.LEVEL_STORAGE + "/Level2.txt");
+	
+	Profile currentUser = new Profile(new File(UserData.USER_FOLDER_LOCATION + "/Mitch.txt"));
+	
+	try {
+		level.saveLevelProgress(currentUser);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	Level levelSave = new Level(Level.LEVEL_STORAGE + "/Mitch_Level2.txt");
+	
+	
 	}
 
 }
