58750f64eb2c9cf0937bd113a70278c7db6bc334
James Colebourn <afroyodacolebourn@gmail.com>
2019-12-06 10:33:46 +0000

    JDoc

--- src/a2/Sprite.java
+++ src/a2/Sprite.java
@@ -1,13 +1,25 @@
 package a2;
-
+/**
+ * Generic type for a sprite, defaulting to the Wall sprite
+ * @author George Williams Walton
+ * @version 1.2
+ */
 public interface Sprite {
 
 	final String DEFAULT_SPRITE = "a2/resources/stock photos/Wall.png";
 	
+	/**
+	 * Sets a default sprite in the event a resource can't be found.
+	 * @return gives the default sprite
+	 */
 	default String getDefaultSprite() {
 		return DEFAULT_SPRITE;
 	}
 	
+	/**
+	 * Getter for default sprite.
+	 * @return gives the default sprite
+	 */
 	default String getSprite() {
 		return getDefaultSprite();
 	};
