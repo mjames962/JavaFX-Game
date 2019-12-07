f5dea3e6d45dce1087d9e84bca80ab8b8a08e80c
mjame <mjame@DESKTOP-7UUV694>
2019-12-07 13:34:20 +0000

    added stuff back in level

--- src/a2/Level.java
+++ src/a2/Level.java
@@ -364,9 +364,60 @@ public class Level {
 			readTeleporter(line);
 			line = in.nextLine();
 		}
+		line = in.nextLine();
+		
+		// player inventory
+		while (!line.equals("*")) {
+			readPlayerInventory(line);
+			line = in.nextLine();
+		}
 		
 		in.close();
 	}
+	
+	private void readPlayerInventory(String line) {
+		Scanner in = new Scanner(line);
+		
+		int redKeys = in.nextInt();
+		int greenKeys = in.nextInt();
+		int blueKeys = in.nextInt();
+		int tokenCount = in.nextInt();
+		int daggerCount = in.nextInt();
+		boolean hasFireBoots = in.nextBoolean();
+		boolean hasFlippers = in.nextBoolean();
+		long time = in.nextLong();
+		int deathCount = in.nextInt();
+		
+		for (int i = 0; i < redKeys; i++) {
+			this.getPlayer().getInventory().add(new RedKey());
+		}
+		
+		for (int i = 0; i < greenKeys; i++) {
+			this.getPlayer().getInventory().add(new GreenKey());
+		}
+		
+		for (int i = 0; i < blueKeys; i++) {
+			this.getPlayer().getInventory().add(new BlueKey());
+		}
+		
+		this.getPlayer().setTokenCount(tokenCount);
+		
+		for (int i = 0; i < daggerCount; i++) {
+			this.getPlayer().getInventory().add(new DaggerItem());
+		}
+		
+		if (hasFireBoots) {
+			this.getPlayer().getInventory().add(new FireBoots());
+		}
+		
+		if (hasFlippers) {
+			this.getPlayer().getInventory().add(new Flippers());
+		}
+		
+		Timer.setSavedTime(time);
+		Level.currentDeaths = deathCount;
+	}
+	
 	/**
 	 * Reads in data for Teleporter Cells.
 	 * @param str is the string being read in for the 
@@ -399,7 +450,7 @@ public class Level {
 		int startY = in.nextInt() - 1;
 		int entityID = in.nextInt();
 
-		String direction = in.next();
+		int direction = in.nextInt();
 
 		Vector2D vector = new Vector2D(startX, startY);
 
@@ -421,6 +472,8 @@ public class Level {
 			case 4:
 				entity = new SmartTargetEnemy(vector, entityID, this);
 				break;
+			case 10:
+				entity = new Dagger(vector, direction);
 			default:
 				entity = null;
 		}
@@ -536,6 +589,9 @@ public class Level {
 		writeTeleporters(saveFilePath);
 
 		writePlayerInventory(saveFilePath);
+		
+		Files.write(Paths.get(saveFilePath), (this.getCurrentDeaths() + "\n*").getBytes(),
+				StandardOpenOption.APPEND);
 
 	}
 
@@ -656,6 +712,7 @@ public class Level {
 		int redKeys = 0;
 		int greenKeys = 0;
 		int blueKeys = 0;
+		int tokenCount = this.getPlayer().getTokenCount();
 		int daggerCount = 0;
 		boolean fireBoots = false;
 		boolean flippers = false;
@@ -684,7 +741,6 @@ public class Level {
 			}
 		}
 		
-		int tokenCount = this.getPlayer().getTokenCount();
 		printLine = printLine + redKeys + " ";
 		printLine = printLine + greenKeys + " ";
 		printLine = printLine + blueKeys + " ";
